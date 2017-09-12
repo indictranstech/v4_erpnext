c48802720daf7f27d316001f1a9ea21e4723b7b8
sagar <sagarshiragawakar@gmail.com>
2017-09-12 17:13:14 +0530

    add init

---
+++ erpnext/hr/doctype/salary_structure/test_salary_structure.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2013, Web Notes Technologies Pvt. Ltd. and Contributors and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+import frappe
+import unittest
+
+test_records = frappe.get_test_records('Salary Structure')
+
+class TestSalaryStructure(unittest.TestCase):
+	pass
